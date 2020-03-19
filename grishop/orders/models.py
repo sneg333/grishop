90c3294404104d0a410d24c94f5f024af134a2d1
sneg333 <igortot@yandex.ru>
2020-03-19 12:46:37 +0300

    grishop_ferst190320

---
+++ grishop/orders/models.py
@@ -0,0 +1,38 @@
+from django.db import models
+from shop.models import Product
+
+
+class Order(models.Model):
+    first_name = models.CharField(max_length=50, verbose_name="имя")
+    last_name = models.CharField(max_length=50, verbose_name="фамилия")
+    email = models.EmailField()
+    address = models.CharField(max_length=250, verbose_name="адрес")
+    postal_code = models.CharField(max_length=20, verbose_name="код")
+    city = models.CharField(max_length=100, verbose_name="город")
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    paid = models.BooleanField(default=False, verbose_name="Выполнено")
+
+    class Meta:
+        ordering = ('-created',)
+        verbose_name = 'Заказ'
+        verbose_name_plural = 'Заказы'
+
+    def __str__(self):
+        return 'Order {}'.format(self.id)
+
+    def get_total_cost(self):
+        return sum(item.get_cost() for item in self.items.all())
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, related_name='items')
+    product = models.ForeignKey(Product, related_name='order_items')
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return '{}'.format(self.id)
+
+    def get_cost(self):
+        return self.price * self.quantity
